c70773e0eb799a17f1f44334d14389c518ec14d4
Damien Cassou <damien@cassou.me>
2019-11-26 21:02:38 +0100

    Fix typos
    Typos found with codespell.

--- memoize.el
+++ memoize.el
@@ -36,7 +36,7 @@
 ;; Memoization takes up memory, which should be freed at some point.
 ;; Because of this, all memoization has a timeout from when the last
 ;; access was. The default timeout is set by
-;; `memoize-default-timeout'.  It can be overriden by using the
+;; `memoize-default-timeout'.  It can be overridden by using the
 ;; `memoize' function, but the `defmemoize' macro will always just use
 ;; the default timeout.
 
